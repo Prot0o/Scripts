b76ad29d98730ce182950a3836dd2ac00cf1363d
Glaxy <dimokdimka@gmail.com>
2016-08-20 21:43:19 +0100

    Update GlaxyStarter.lua

--- GlaxyStarter.lua
+++ GlaxyStarter.lua
@@ -583,8 +583,6 @@ function AutoUpdate()
 				Print("New version available "..ServerVersion);
 				Print(">>Updating, please don't press F9<<");
 				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () Print("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3);
-			else
-				CustomLoad();
 			end
 		else
 			Print("Error while downloading version info");
